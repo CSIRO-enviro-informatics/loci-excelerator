5ef35cd36c41454fd0e8cafc8dc93eacbda465cd
Seaton, Shane (L&W, Black Mountain) <shane.seaton@csiro.au>
2019-05-07 09:10:54 +1000

    Adding sb-admin-2 styling

--- excelerator/client/main.js
+++ excelerator/client/main.js
@@ -1,6 +1,11 @@
 import popper from "popper.js";
 global.Popper = popper;
 
+import '@fortawesome/fontawesome-free/css/all.css'
+// window.FontAwesomeConfig = { autoReplaceSvg: false }
+import "startbootstrap-sb-admin-2/vendor/bootstrap/js/bootstrap.bundle.min.js"
+import "startbootstrap-sb-admin-2/vendor/jquery-easing/jquery.easing.min.js"
+import "startbootstrap-sb-admin-2/js/sb-admin-2.js"
 
 // Client entry point, imports all client code
 
---
+++ excelerator/postinstall.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cp -R node_modules/@fortawesome/fontawesome-free/webfonts ./public/
