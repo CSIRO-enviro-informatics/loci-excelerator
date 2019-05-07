d68734eb963ddf125066affd8a8977cfd5dc95db
Seaton, Shane (L&W, Black Mountain) <shane.seaton@csiro.au>
2019-05-07 13:20:51 +1000

    WIP: Adding Job creator component

--- excelerator/imports/ui/components/jobCreator/jobCreator.js
+++ excelerator/imports/ui/components/jobCreator/jobCreator.js
@@ -1,17 +1,17 @@
-import './hello.html';
+import './jobCreator.html';
 
-Template.hello.onCreated(function helloOnCreated() {
+Template.jobCreator.onCreated(function helloOnCreated() {
   // counter starts at 0
   this.counter = new ReactiveVar(0);
 });
 
-Template.hello.helpers({
+Template.jobCreator.helpers({
   counter() {
     return Template.instance().counter.get();
   },
 });
 
-Template.hello.events({
+Template.jobCreator.events({
   'click button'(event, instance) {
     // increment the counter when button is clicked
     instance.counter.set(instance.counter.get() + 1);
