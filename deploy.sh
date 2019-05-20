3e414fbf6294f15b7ed3bcbb25aa21949c84fee9
Seaton, Shane (L&W, Black Mountain) <shane.seaton@csiro.au>
2019-05-20 17:37:39 +1000

    Update to use npx to avoid global installs in jenkins

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,8 @@ set +x
 
 # export SSH_KEY_PATH="/Users/sea066/secrets/Loc-I1.pem"
 
-if ! [ -x "$(command -v mup)" ]; then
-    npm install -g mup
-fi
-
+npm install mup
+ 
 cd .deploy
-mup setup
-mup deploy
+npx mup setup
+npx mup deploy
