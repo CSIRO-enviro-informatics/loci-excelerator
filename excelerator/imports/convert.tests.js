5fd72fe18e18f15c45d3f4dad3f03141b119a823
Seaton, Shane (L&W, Black Mountain) <shane.seaton@csiro.au>
2019-06-02 00:44:48 +1000

    Start of testing

---
+++ excelerator/imports/convert.tests.js
@@ -0,0 +1,9 @@
+import chai from 'chai';
+
+describe('Conversions', function () {
+    it('can convert addresses to MB (sfWithin)', function () {
+        // This code will be executed by the test driver when the app is started
+        // in the correct mode
+        chai.assert.equal(1,0);
+    })
+})
