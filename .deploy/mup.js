01913799c167969726e8786a3bc0aa1bb2481f2c
Seaton, Shane (L&W, Black Mountain) <shane.seaton@csiro.au>
2019-09-03 13:40:56 +1000

    Revert mongo version

--- .deploy/mup.js
+++ .deploy/mup.js
@@ -10,7 +10,6 @@ module.exports = {
   app: {
     name: 'Excelerator',
     path: '../excelerator/',
-    deployCheckWaitTime: 300,
 
     servers: {
       one: {},
@@ -33,17 +32,15 @@ module.exports = {
 
     docker: {
       image: 'abernix/meteord:node-8.4.0-base',
-      prepareBundle: true,
-      stopAppDuringPrepareBundle: true,
     },
 
     // Show progress bar while uploading bundle to server
     // You might need to disable it on CI servers
-    enableUploadProgressBar: false
+    enableUploadProgressBar: true
   },
 
   mongo: {
-    version: '4.0.2',
+    version: '3.4.1',
     servers: {
       one: {}
     }
